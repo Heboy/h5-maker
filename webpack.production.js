8041954f8917ea214101a86a130d0031412e0547
壮实 <haibo.thb@alibaba-inc.com>
2016-10-12 19:57:49 +0800

    update webpack and rebuild

--- webpack.production.js
+++ webpack.production.js
@@ -1,37 +1,48 @@
 /**
  * Created by Soup Tang on 2015/8/26.
  */
-var webpack = require("webpack");
+var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './src/components/App.js',
-        vendor: ["react", "react-dom"],
-        immutable: ["immutable"]
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: '[name].js'
-    },
-    module: {
-        loaders: [
-            {test: /\.css$/, loader: 'style!css'},
-            {test: /.(png|jpg)$/, loader: "url-loader?limit=100000"},
-            {test: /\.js$/, loader: "babel-loader", query: {presets: ['react', 'es2015']}},
-            {test: /\.scss$/, loader: "style!css!sass"}
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify("production")
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            names: ['vendor', 'immutable']
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
-            mangle: false
-        })
-
+  entry: {
+    app: './src/components/App.jsx',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+    publicPath: './dist/',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /.(png|jpg)$/, loader: 'url-loader?limit=100000' },
+      { test: /\.js$/, loader: 'babel-loader', query: { presets: ['react', 'es2015'] } },
+      { test: /\.scss$/, loader: 'style!css!sass' }
     ]
+  },
+  externals: [{
+    'react': 'React'
+  }, {
+    'react-dom': 'ReactDOM'
+  }, {
+    'immutable': 'Immutable'
+  }],
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /.(png|jpg)$/, loader: 'url-loader?limit=100000' },
+      { test: /\.(js|jsx)$/, loader: 'babel-loader', query: { presets: ['react', 'es2015'] } },
+      { test: /\.scss$/, loader: 'style!css!sass' }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      mangle: false
+    })
+  ]
 };
